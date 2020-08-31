043c4841558f27f2b2690b411410cfc58de16371
Marcos Vinicius <mviniciusgc@hotmail.com>
2020-08-31 11:25:50 -0300

    trocando descrição dos testes

--- __tests__/modules/cartesianPlane/services/CalculateDistance.spec.ts
+++ __tests__/modules/cartesianPlane/services/CalculateDistance.spec.ts
@@ -4,7 +4,7 @@ import { CartesianPlaneRepository } from '../repositories/CartesianPlane';
 import { CalculateDistanceService } from '@modules/cartesianPlane/services/CalculateDistanceService';
 import { CartesianPlaneDTO } from '@modules/cartesianPlane/dto/CartesianPlane';
 
-describe("Calcular a distancia", () => {
+describe("Calculate the distance", () => {
 
     let fakeCartesianPlaneRepository: CartesianPlaneRepository;
     let calculateDistanceService: CalculateDistanceService;
@@ -14,7 +14,7 @@ describe("Calcular a distancia", () => {
         calculateDistanceService = new CalculateDistanceService(fakeCartesianPlaneRepository);
     })
 
-    it('deve calcular a distancia apartir de quatro valores dos eixo x e Y no placo cartesiano', async () => {
+    it('should calculate the distance from four values ​​of the x and y axis on the Cartesian board', async () => {
 
         const pontosPlanoCatesiano1 = new CartesianPlaneDTO(1.0, 7.0, 5.0, 9.0);
         const pontosPlanoCatesiano2 = new CartesianPlaneDTO(-2.5, 0.4, 12.1, 7.3);
